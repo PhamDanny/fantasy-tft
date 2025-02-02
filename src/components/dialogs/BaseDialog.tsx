b80fb7ee1c56b3c456d90515c1b557dd56bbdb67
Danny Pham <dpham1006@gmail.com>
2025-02-01 20:35:08 -0800

    removed extraneous basedialog

--- src/components/dialogs/BaseDialog.tsx
+++
@@ -1,8 +0,0 @@
-<div className="modal-dialog modal-dialog-scrollable mx-2 mx-md-auto" 
-  style={{ 
-    maxWidth: '600px',
-    margin: '1.75rem auto',
-    '@media (max-width: 768px)': {
-      margin: '1rem'
-    }
-  }}> 
